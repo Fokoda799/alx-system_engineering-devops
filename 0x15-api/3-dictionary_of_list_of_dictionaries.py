79560f4782aee05d253b9a360b81dd8de10072a4
Fokoda799 <feyt2003@gmail.com>
2024-04-25 20:57:32 +0100

    ok

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Dictionary of list of dictionaries"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    users = requests.get("http://jsonplaceholder.typicode.com/users").json()
+    tasks = requests.get("http://jsonplaceholder.typicode.com/todos").json()
+    all_users = {}
+    all_tasks = []
+
+    for user in users:
+        for task in tasks:
+            if (task.get("userId") == int(user['id'])):
+                temp = {}
+                temp["task"] = task.get("title")
+                temp["completed"] = task.get("completed")
+                temp["username"] = user.get("username")
+                all_tasks.append(temp)
+        all_users[user['id']] = all_tasks
+
+    with open("todo_all_employees.json", 'w+') as jsonfile:
+        json.dump(all_users, jsonfile)
