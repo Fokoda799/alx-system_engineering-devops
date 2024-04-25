d4a9f7724c791a9e324737632037c5d645ba0903
Fokoda799 <feyt2003@gmail.com>
2024-04-25 13:38:22 +0100

    ok

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""Gather data from
-an API
-"""
-import requests
-import sys
-import json
+"""Gather data froman API"""
 
-em_id = sys.argv[1]
-url_1 = f"https://jsonplaceholder.typicode.com/users/{em_id}"
-url_2 = f"https://jsonplaceholder.typicode.com/users/{em_id}/todos"
-user = requests.get(url_1)
-todo = requests.get(url_2)
+if __name__ == "__main__":
+    import requests
+    import sys
+    import json
 
-employee = user.json()
-todos = todo.json()
+    if len(sys.argv) != 2:
+        exit(1)
 
-em_name = employee["name"]
-total_tasks = len(todos)
-tasks = []
+    em_id = sys.argv[1]
+    url_1 = f"https://jsonplaceholder.typicode.com/users/{em_id}"
+    url_2 = f"https://jsonplaceholder.typicode.com/users/{em_id}/todos"
+    user = requests.get(url_1)
+    todo = requests.get(url_2)
 
-for task in todos:
-    if task["completed"] is True:
-        tasks.append(task["title"])
-done_tasks = len(tasks)
+    employee = user.json()
+    todos = todo.json()
 
-print(f"Employee {em_name} is done with tasks({done_tasks}/{total_tasks}):")
-for task in tasks:
-    print(f"\t{task}")
+    em_name = employee["name"]
+    total_tasks = len(todos)
+    tasks = []
+
+    for task in todos:
+        if task["completed"] is True:
+            tasks.append(task["title"])
+    done_tasks = len(tasks)
+
+    print(f"Employee {em_name} is done with tasks({done_tasks}/{total_tasks}):")
+    for task in tasks:
+        print(f"\t{task}")
