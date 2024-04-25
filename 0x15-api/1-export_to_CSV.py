39c723adfd87c55602336d831db3b88b6be7cb90
Fokoda799 <feyt2003@gmail.com>
2024-04-25 19:55:13 +0100

    fix code

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python3
 """Export to CSV"""
 import csv
-import json
 import requests
 import sys
 
 
 def main():
     if len(sys.argv) != 2:
+        print("Usage: python script.py <user_id>")
         exit(1)
 
     user_id = sys.argv[1]
     user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    todos_url = user_url + "/todos"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
 
     user_re = requests.get(user_url)
     todos_re = requests.get(todos_url)
+
+    if user_re.status_code != 200 or todos_re.status_code != 200:
+        print("Failed to retrieve data from the API.")
+        exit(1)
+
     user = user_re.json()
     todos = todos_re.json()
-
     username = user['username']
+    all_tasks = []
 
-    with open(f"{user_id}.csv", 'w', newline="") as file:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
+    for task in todos:
+        temp = []
+        temp.extend((user_id,
+                     username,
+                     task.get("completed"),
+                     task.get("title")))
+        all_tasks.append(temp)
 
-        for task in todos:
-            writer.writerow({
-                "USER_ID": user_id,
-                "USERNAME": username,
-                "TASK_STATUS": task['completed'],
-                "TASK_TITLE": task['title']
-            })
+    with open("{}.csv".format(user_id), 'w+') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerows(all_tasks)
 
 
 if __name__ == "__main__":
