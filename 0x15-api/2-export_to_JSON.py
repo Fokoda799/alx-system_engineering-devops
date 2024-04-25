31bce7996c0689bc720f74a7a24e4522cb637eed
Fokoda799 <feyt2003@gmail.com>
2024-04-25 20:22:57 +0100

    ok

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,40 +1,26 @@
 #!/usr/bin/python3
-"""Export to JSON"""
+"""
+Extend the python script from exercise 0 to export data in JSON format.
+File name must be: USER_ID.json
+"""
 import json
 import requests
 import sys
 
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <user_id>")
-        exit(1)
-
-    user_id = sys.argv[1]
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-
-    user_re = requests.get(user_url)
-    todos_re = requests.get(todos_url)
-
-    if user_re.status_code != 200 or todos_re.status_code != 200:
-        print("Failed to retrieve data from the API.")
-        exit(1)
-
-    user = user_re.json()
-    todos = todos_re.json()
-    username = user['username']
-    all_tasks = []
-
-    format = {f'{user_id}': [{
-                                "task": f"{task['title']}",
-                                "completed": f"{task['completed']}",
-                                "username": f"{username}"
-                             } for task in todos]}
-
-    with open("{}.json".format(user_id), 'w+') as jsonfile:
-        json.dump(format, jsonfile)
-
-
 if __name__ == "__main__":
-    main()
+    emp_id = sys.argv[1]
+    username = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                            .format(emp_id)).json().get("username")
+    all_tasks = []
+    r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
+
+    for task in r:
+        if (task.get("userId") == int(emp_id)):
+            temp = {}
+            temp["task"] = task.get("title")
+            temp["completed"] = task.get("completed")
+            temp["username"] = username
+            all_tasks.append(temp)
+
+    with open("{}.json".format(emp_id), 'w+') as jsonfile:
+        json.dump({emp_id: all_tasks}, jsonfile)
